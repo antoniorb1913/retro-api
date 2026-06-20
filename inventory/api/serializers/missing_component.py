e0e710033afeebfbd951580675edf14704862c5a
antoniorb1913 <136896845+antoniorb1913@users.noreply.github.com>
2026-06-20 16:43:41 +0200

    Delete inventory/api/serializers directory

--- inventory/api/serializers/missing_component.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from inventory.models.Missing_component import MissingComponent
-
-class MissingComponentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MissingComponent
-        fields = ['id', 'name']
